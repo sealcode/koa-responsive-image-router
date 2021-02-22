1c978bbf10218aed63a3fd6a1c0eba49447e604d
Arkadiusz Wieczorek <arkadiusz.wieczorek@sealcode.org>
2021-02-22 22:10:43 +0100

    Update

--- example/app.js
+++ example/app.js
@@ -1,64 +1,122 @@
 import Koa from "koa";
 import Router from "koa-router";
-import fs from "fs";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import sharp from "sharp";
 import crypto from "crypto";
+import { statSync, readFileSync, closeSync, openSync } from "fs";
+import { access, mkdir, copyFile } from "fs/promises";
+import { dirname, extname, basename } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = new Koa();
 const router = new Router();
 
-// user settings
-const tmp_dir = "/tmp/image-cache";
+const temp_dir = "/tmp/image-cache";
 const resolutions = [320, 600, 900, 1600, 1920];
-const original_file = {
-    filename: "image",
-    type: "png",
-    path: ".",
-};
-
-// init router
-const hashedName = generateImageName({
-    filename: original_file.filename,
-    type: original_file.type,
-});
+const original_file_path = "./image.png";
+const hash = getHash(original_file_path);
 
 async function init() {
-    const { filename, type } = original_file;
-    const resolution = resolutions[resolutions.length - 1];
-
-    await getImage({ filename, resolution, type });
-    try {
-        return getCachedImage({ filename, resolution, type });
-    } catch {
-        const buffer = await generateImage({ filename, resolution, type });
-        saveImage({ filename, resolution, type, buffer });
-        return buffer;
-    }
+    await generateDirectory();
+    await copySourceFile();
 }
 init();
 
-// router.get("/image", async (ctx, next) => {
-//     const image = await fs.readFile(
-//         `${__dirname}/image.png`,
-//         "utf8",
-//         (err, data) => data
-//     );
-//     ctx.body = `<img src="${path.resolve(__dirname, "image.png")}"
-// 	alt="Image alt"
-// 	srcset="/files/16864/clock-demo-200px.png 1x, /files/16797/clock-demo-400px.png 2x">`;
-// });
+function getHash(original_file_path) {
+    return crypto
+        .createHash("md5")
+        .update(
+            basename(original_file_path) +
+                statSync(`${__dirname}/${original_file_path}`).atime
+        )
+        .digest("hex");
+}
 
-// image_router.image()
+function getHashedPath(hash) {
+    return `${__dirname}${temp_dir}/${hash}`;
+}
+
+async function generateDirectory() {
+    const destination = `${__dirname}/${temp_dir}/${hash}`;
+    try {
+        await access(destination);
+    } catch {
+        try {
+            await mkdir(destination, { recursive: true });
+        } catch (error) {
+            console.log("directory exist");
+        }
+    }
+}
+
+async function copySourceFile() {
+    const source = `${__dirname}/${basename(original_file_path)}`;
+    const destination = `${getHashedPath(hash)}/original-file`;
+    await copyFile(source, destination);
+}
+
+router.get("/images/:hash/:filename", async (ctx) => {
+    const { hash, filename } = ctx.params;
+    const resolution = parseInt(filename.split(".")[0]);
+    const destination = `${getHashedPath(hash)}`;
+    const type = extname(filename).split(".").pop();
+
+    if (resolutions.find((el) => el === resolution)) {
+        try {
+            await access(destination);
+            ctx.body = await getImage({ hash, resolution, type });
+            ctx.type = `image/${ctx.params.type}`;
+        } catch (error) {
+            ctx.response.status = 404;
+        }
+    } else {
+        ctx.response.status = 404;
+    }
+});
+
+async function getImage({ hash, resolution, type }) {
+    try {
+        return getCachedImage({ hash, resolution, type });
+    } catch {
+        const buffer = await generateImage({ hash, resolution, type });
+        saveImage({ hash, resolution, type, buffer });
+        return buffer;
+    }
+}
+
+function getCachedImage({ hash, resolution, type }) {
+    const file = readFileSync(`${getHashedPath(hash)}/${resolution}.${type}`);
+    return Buffer.from(file, "base64");
+}
+
+async function saveImage({ hash, resolution, type, buffer }) {
+    await createEmptyImage({ hash, type, resolution });
+    sharp(buffer).toFile(`${getHashedPath(hash)}/${resolution}.${type}`);
+}
+
+async function createEmptyImage({ hash, type, resolution }) {
+    closeSync(openSync(`${getHashedPath(hash)}/${resolution}.${type}`, "w"));
+}
+
+async function generateImage({ hash, resolution, type }) {
+    return await sharp(`${getHashedPath(hash)}/original-file`)
+        .resize(parseInt(resolution))
+        .toFormat(type)
+        .toBuffer();
+}
+
+app.use(router.routes()).use(router.allowedMethods()).listen(3005);
+
+// tbc
+
 function image({ path, resolutions, quality, extensions, alt }) {
     let sources = "";
 
     for (let i = 0; i < resolutions.length; i++) {
         for (let j = 0; j < extensions.length; j++) {
-            const filename = `${hashedName}-${resolutions[i]}.${extensions[j]}`;
+            const filename = `${hash}-${resolutions[i]}.${extensions[j]}`;
             sources += `<source srcset="${path}/${filename}" media="(min-width: ${resolutions[i]}px)">`;
         }
     }
@@ -69,69 +127,3 @@ function image({ path, resolutions, quality, extensions, alt }) {
             <img src="" alt="">
         </picture>`;
 }
-
-router.get("/images/:filename/:resolution/:type", async (ctx) => {
-    if (
-        resolutions.find(
-            (resolution) => resolution === parseInt(ctx.params.resolution)
-        )
-    ) {
-        ctx.body = await getImage(ctx.params);
-        ctx.type = `image/${ctx.params.type}`;
-    } else {
-        ctx.response.status = 404;
-    }
-});
-
-async function getImage({ filename, resolution, type }) {
-    try {
-        return getCachedImage({ resolution, type });
-    } catch {
-        const buffer = await generateImage({ filename, resolution, type });
-        saveImage({ resolution, type, buffer });
-        return buffer;
-    }
-}
-
-function generateImageName({ filename, type }) {
-    // get original file details
-    const file_details = fs.statSync(`${__dirname}/${filename}.${type}`);
-    return crypto
-        .createHash("md5")
-        .update(filename + file_details.atime)
-        .digest("hex");
-}
-
-function getCachedImage({ resolution, type }) {
-    const file = fs.readFileSync(
-        `${__dirname}/tmp/image-cache/${hashedName}-${resolution}.${type}`
-    );
-    return Buffer.from(file, "base64");
-}
-
-async function saveImage({ resolution, type, buffer }) {
-    await createEmptyImage({ type, resolution });
-    sharp(buffer).toFile(
-        `${__dirname}/tmp/image-cache/${hashedName}-${resolution}.${type}`
-    );
-}
-
-async function createEmptyImage({ type, resolution }) {
-    fs.closeSync(
-        fs.openSync(
-            `${__dirname}/tmp/image-cache/${hashedName}-${resolution}.${type}`,
-            "w"
-        )
-    );
-}
-
-async function generateImage({ filename, type, resolution }) {
-    // get original image
-    const pixelArray = await sharp(`${__dirname}/${filename}.${type}`)
-        .toResize(parseInt(resolution))
-        .toFormat(type)
-        .toBuffer();
-    return pixelArray;
-}
-
-app.use(router.routes()).use(router.allowedMethods()).listen(3001);
