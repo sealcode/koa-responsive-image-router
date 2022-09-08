ff88237dc56fedd357e4f41efceefd5a1bd848b8
Kuba Orlik <kontakt@kuba-orlik.name>
2022-09-08 16:29:05 +0200

    Update types

--- @types/guessResolutions.d.ts
+++ @types/guessResolutions.d.ts
@@ -1,9 +1,6 @@
 declare type resolutionGuessOptions = {
-	min_viewport_size?: number;
-	max_viewport_size?: number;
+    min_viewport_size?: number;
+    max_viewport_size?: number;
 };
-declare const guessResolutions: (
-	sizes_attr: string,
-	{ min_viewport_size, max_viewport_size }?: resolutionGuessOptions
-) => number[];
+declare const guessResolutions: (sizes_attr: string, { min_viewport_size, max_viewport_size, }?: resolutionGuessOptions) => number[];
 export { guessResolutions };
--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,37 +3,28 @@ import Router from "@koa/router";
 import sharp from "sharp";
 import { Middleware } from "koa";
 export default class KoaResponsiveImageRouter extends Router {
-	static_path: string;
-	router: Router;
-	hashToResolutions: Record<string, number[]>;
-	hashToLossless: Record<string, boolean>;
-	hashToMetadata: Record<string, Promise<sharp.Metadata> | undefined>;
-	hashToOriginalPath: Record<string, string>;
-	nginxWarningDisplayed: boolean;
-	constructor(static_path: string);
-	getMetadata(hash: string): Promise<sharp.Metadata>;
-	private makeImageURL;
-	makeNginxConfig(cache_path: string, max_size_mb: number): string;
-	image({
-		resolutions,
-		sizes_attr,
-		path,
-		lossless,
-		lazy,
-		img_style,
-		target_ratio,
-		ratio_diff_threshold,
-	}: {
-		resolutions?: number[];
-		sizes_attr: string;
-		path: string;
-		lossless?: boolean;
-		lazy?: boolean;
-		img_style?: string;
-		target_ratio?: number;
-		ratio_diff_threshold?: number;
-	}): Promise<string>;
-	getRoutes(): Middleware;
-	private getHash;
-	private generateImage;
+    static_path: string;
+    router: Router;
+    hashToResolutions: Record<string, number[]>;
+    hashToLossless: Record<string, boolean>;
+    hashToMetadata: Record<string, Promise<sharp.Metadata> | undefined>;
+    hashToOriginalPath: Record<string, string>;
+    nginxWarningDisplayed: boolean;
+    constructor(static_path: string);
+    getMetadata(hash: string): Promise<sharp.Metadata>;
+    private makeImageURL;
+    makeNginxConfig(cache_path: string, max_size_mb: number): string;
+    image({ resolutions, sizes_attr, path, lossless, lazy, img_style, target_ratio, ratio_diff_threshold, }: {
+        resolutions?: number[];
+        sizes_attr: string;
+        path: string;
+        lossless?: boolean;
+        lazy?: boolean;
+        img_style?: string;
+        target_ratio?: number;
+        ratio_diff_threshold?: number;
+    }): Promise<string>;
+    getRoutes(): Middleware;
+    private getHash;
+    private generateImage;
 }
